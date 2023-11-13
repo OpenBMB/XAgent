8625be6fbaee5085ac3d38dc0efc3cfedc173f34
luchun <71970539+zhanghy-sketchzh@users.noreply.github.com>
2023-11-13 14:08:09 +0800

    Update openai.py
    Add feature comment

--- XAgent/ai_functions/request/openai.py
+++ XAgent/ai_functions/request/openai.py
@@ -5,6 +5,15 @@ import pkg_resources
 
 
 def chat_completion_to_dict(chat_completion_instance):
+    """
+    Convert the output format of openai in version 1.x to the output format of openai in version 0.28.0
+
+    Args:
+        chat_completion_instance: Represents a chat completion response returned by 1.x openai model, based on the provided input.
+
+    Return:
+        Version 0.28.0 of openai's chat completion return format in dictionary form
+    """
     choices = chat_completion_instance.choices[0]
     finish_reason = choices.finish_reason
     index = choices.index
@@ -59,11 +68,14 @@ def chat_completion_to_dict(chat_completion_instance):
     }
     return response
 
+
 def chatcompletion_request(**kwargs):
     model_name = get_model_name(
         kwargs.pop("model", CONFIG.default_completion_kwargs["model"])
     )
     logger.debug("chatcompletion: using " + model_name)
+    
+    # Get model-related configuration parameters
     chatcompletion_kwargs = get_apiconfig_by_model(model_name)
 
     if pkg_resources.parse_version(
@@ -78,12 +90,16 @@ def chatcompletion_request(**kwargs):
             base_url = chatcompletion_kwargs.pop("api_base", None)
         api_key = chatcompletion_kwargs.pop("api_key", None)
         chatcompletion_kwargs.update(kwargs)
+
+        # Create OpenAI client
         client = OpenAI(api_key=api_key, base_url=base_url)
 
         try:
+            # Send a Chat Completion request and convert the result into a dictionary
             response = chat_completion_to_dict(
                 client.chat.completions.create(**chatcompletion_kwargs)
             )
+            # Check if the context length limit is exceeded
             if response["choices"][0]["finish_reason"] == "length":
                 raise BadRequestError("maximum context length exceeded", None)
         except BadRequestError as e:
@@ -106,10 +122,10 @@ def chatcompletion_request(**kwargs):
 
                 response = chat_completion_to_dict(
                     client.chat.completions.create(**chatcompletion_kwargs)
-                    )
+                )
             else:
                 raise e
-            
+
     else:
         import openai
         from openai.error import InvalidRequestError
@@ -122,21 +138,21 @@ def chatcompletion_request(**kwargs):
             if response["choices"][0]["finish_reason"] == "length":
                 raise InvalidRequestError("maximum context length exceeded", None)
         except InvalidRequestError as e:
-            if 'maximum context length' in e._message:
-                if model_name == 'gpt-4':
-                    if 'gpt-4-32k' in CONFIG.api_keys:
-                        model_name = 'gpt-4-32k'
+            if "maximum context length" in e._message:
+                if model_name == "gpt-4":
+                    if "gpt-4-32k" in CONFIG.api_keys:
+                        model_name = "gpt-4-32k"
                     else:
-                        model_name = 'gpt-3.5-turbo-16k'
-                elif model_name == 'gpt-3.5-turbo':
-                    model_name = 'gpt-3.5-turbo-16k'
+                        model_name = "gpt-3.5-turbo-16k"
+                elif model_name == "gpt-3.5-turbo":
+                    model_name = "gpt-3.5-turbo-16k"
                 else:
                     raise e
                 print("max context length reached, retrying with " + model_name)
                 chatcompletion_kwargs = get_apiconfig_by_model(model_name)
                 chatcompletion_kwargs.update(kwargs)
-                chatcompletion_kwargs.pop('schema_error_retry', None)
-                
+                chatcompletion_kwargs.pop("schema_error_retry", None)
+
                 response = openai.ChatCompletion.create(**chatcompletion_kwargs)
                 response = json.loads(str(response))
             else:

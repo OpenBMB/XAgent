07f44e7655eb08ea74e3e446532bf3b484061155
luchun <71970539+zhanghy-sketchzh@users.noreply.github.com>
2023-11-20 15:18:26 +0800

    Add: Add unit tests for OpenAI chatcompletion_request and refactor code for OpenAI v1.x compatibility. (#216)
    * openai
    
    * Update openai.py

--- XAgent/ai_functions/request/openai.py
+++ XAgent/ai_functions/request/openai.py
@@ -1,31 +1,44 @@
 import json
 import openai
-
 from XAgent.logs import logger
 from XAgent.config import CONFIG, get_apiconfig_by_model, get_model_name
 
-from tenacity import retry, stop_after_attempt, wait_exponential, retry_if_not_exception_type, wait_chain, wait_none
-import pkg_resources
-if pkg_resources.get_distribution("openai").version < "1.0":
+from tenacity import (
+    retry,
+    stop_after_attempt,
+    wait_exponential,
+    retry_if_not_exception_type,
+    wait_chain,
+    wait_none,
+)
+import importlib.metadata as metadata
+
+if metadata.version("openai") < "1.0":
     from openai.error import AuthenticationError, PermissionError, InvalidRequestError
-    RETRY_ERRORS = (AuthenticationError, PermissionError,
-                    InvalidRequestError, AssertionError)
+
+    RETRY_ERRORS = (
+        AuthenticationError,
+        PermissionError,
+        InvalidRequestError,
+        AssertionError,
+    )
 
     @retry(
         retry=retry_if_not_exception_type(RETRY_ERRORS),
-        stop=stop_after_attempt(CONFIG.max_retry_times+3),
-        wait=wait_chain(*[wait_none() for _ in range(3)] +
-                        [wait_exponential(min=61, max=293)]),
+        stop=stop_after_attempt(CONFIG.max_retry_times + 3),
+        wait=wait_chain(
+            *[wait_none() for _ in range(3)] + [wait_exponential(min=61, max=293)]
+        ),
         reraise=True,
     )
     def chatcompletion_request(**kwargs):
         """Handle operation of OpenAI chat completion.
 
-        This function operates OpenAI chat completion with provided 
-        arguments. It gets the model name, applies a JSON web token, if the 
-        response indicates the context length has been exceeded, it attempts 
-        to get a higher-capacity language model if it exists in the configuration 
-        and reattempts the operation. Otherwise, it will raise an error message. 
+        This function operates OpenAI chat completion with provided
+        arguments. It gets the model name, applies a JSON web token, if the
+        response indicates the context length has been exceeded, it attempts
+        to get a higher-capacity language model if it exists in the configuration
+        and reattempts the operation. Otherwise, it will raise an error message.
 
         Args:
             **kwargs: Variable length argument list including (model:str, etc.).
@@ -34,11 +47,12 @@ if pkg_resources.get_distribution("openai").version < "1.0":
             dict: chat completion response.
 
         Raises:
-            InvalidRequestError: If any error occurs during chat completion operation or 
+            InvalidRequestError: If any error occurs during chat completion operation or
             context length limit exceeded and no fallback models available.
         """
-        model_name = get_model_name(kwargs.pop(
-            'model', CONFIG.default_completion_kwargs['model']))
+        model_name = get_model_name(
+            kwargs.pop("model", CONFIG.default_completion_kwargs["model"])
+        )
         logger.debug("chatcompletion: using " + model_name)
         chatcompletion_kwargs = get_apiconfig_by_model(model_name)
         chatcompletion_kwargs.update(kwargs)
@@ -46,27 +60,25 @@ if pkg_resources.get_distribution("openai").version < "1.0":
         try:
             response = openai.ChatCompletion.create(**chatcompletion_kwargs)
             response = json.loads(str(response))
-            if response['choices'][0]['finish_reason'] == 'length':
-                raise InvalidRequestError(
-                    'maximum context length exceeded', None)
+            if response["choices"][0]["finish_reason"] == "length":
+                raise InvalidRequestError("maximum context length exceeded", None)
         except InvalidRequestError as e:
-            if 'maximum context length' in e._message:
-                if model_name == 'gpt-4':
-                    if 'gpt-4-32k' in CONFIG.api_keys:
-                        model_name = 'gpt-4-32k'
+            if "maximum context length" in e._message:
+                if model_name == "gpt-4":
+                    if "gpt-4-32k" in CONFIG.api_keys:
+                        model_name = "gpt-4-32k"
                     else:
-                        model_name = 'gpt-3.5-turbo-16k'
-                elif model_name == 'gpt-3.5-turbo':
-                    model_name = 'gpt-3.5-turbo-16k'
+                        model_name = "gpt-3.5-turbo-16k"
+                elif model_name == "gpt-3.5-turbo":
+                    model_name = "gpt-3.5-turbo-16k"
                 else:
                     raise e
                 print("max context length reached, retrying with " + model_name)
                 chatcompletion_kwargs = get_apiconfig_by_model(model_name)
                 chatcompletion_kwargs.update(kwargs)
-                chatcompletion_kwargs.pop('schema_error_retry', None)
+                chatcompletion_kwargs.pop("schema_error_retry", None)
 
-                response = openai.ChatCompletion.create(
-                    **chatcompletion_kwargs)
+                response = openai.ChatCompletion.create(**chatcompletion_kwargs)
                 response = json.loads(str(response))
             else:
                 raise e
@@ -75,21 +87,48 @@ if pkg_resources.get_distribution("openai").version < "1.0":
 
 else:
     from openai import AuthenticationError, PermissionDeniedError, BadRequestError
-    RETRY_ERRORS = (AuthenticationError, PermissionDeniedError,
-                    BadRequestError, AssertionError)
+
+    RETRY_ERRORS = (
+        AuthenticationError,
+        PermissionDeniedError,
+        BadRequestError,
+        AssertionError,
+    )
+
     @retry(
         retry=retry_if_not_exception_type(RETRY_ERRORS),
-        stop=stop_after_attempt(CONFIG.max_retry_times+3), 
-        wait=wait_chain(*[wait_none() for _ in range(3)]+[wait_exponential(min=61, max=293)]),
-        reraise=True,)
+        stop=stop_after_attempt(CONFIG.max_retry_times + 3),
+        wait=wait_chain(
+            *[wait_none() for _ in range(3)] + [wait_exponential(min=61, max=293)]
+        ),
+        reraise=True,
+    )
     def chatcompletion_request(**kwargs):
+        """Handle operation of OpenAI v1.x.x chat completion.
+
+        This function operates OpenAI v1.x.x chat completion with provided
+        arguments. It gets the model name, applies a JSON web token, if the
+        response indicates the context length has been exceeded, it attempts
+        to get a higher-capacity language model if it exists in the configuration
+        and reattempts the operation. Otherwise, it will raise an error message.
+
+        Args:
+            **kwargs: Variable length argument list including (model:str, etc.).
+
+        Returns:
+            response (dict): A dictionary containing the response from the Chat API.
+            The structure of the dictionary is based on the API response format.
+
+        Raises:
+            BadRequestError: If any error occurs during chat completion operation or
+            context length limit exceeded and no fallback models available.
+        """
         model_name = get_model_name(
             kwargs.pop("model", CONFIG.default_completion_kwargs["model"])
         )
         logger.debug("chatcompletion: using " + model_name)
         chatcompletion_kwargs = get_apiconfig_by_model(model_name)
 
-        
         request_timeout = kwargs.pop("request_timeout", 60)
         if "base_url" in chatcompletion_kwargs:
             base_url = chatcompletion_kwargs.pop("base_url", None)
@@ -97,11 +136,37 @@ else:
             base_url = chatcompletion_kwargs.pop("api_base", None)
         api_key = chatcompletion_kwargs.pop("api_key", None)
         chatcompletion_kwargs.update(kwargs)
-        client =  openai.OpenAI(api_key=api_key, base_url=base_url,timeout=request_timeout)
+        client = openai.OpenAI(
+            api_key=api_key, base_url=base_url, timeout=request_timeout
+        )
+        try:
+            completions = client.chat.completions.create(**chatcompletion_kwargs)
+            response = completions.model_dump()
+            if response["choices"][0]["finish_reason"] == "length":
+                raise BadRequestError(
+                    message="maximum context length exceeded", response=None, body=None
+                )
 
-        completions = client.chat.completions.create(**chatcompletion_kwargs)
-        response = completions.model_dump()
-        if response["choices"][0]["finish_reason"] == "length":
-            raise BadRequestError("maximum context length exceeded", None)
-            
+        except BadRequestError as e:
+            if "maximum context length" in e.message:
+                if model_name == "gpt-4" and "gpt-4-32k" in CONFIG.api_keys:
+                    model_name = "gpt-4-32k"
+                else:
+                    model_name = "gpt-3.5-turbo-16k"
+
+                print(f"max context length reached, retrying with {model_name}")
+                chatcompletion_kwargs = get_apiconfig_by_model(model_name)
+                request_timeout = kwargs.pop("request_timeout", 60)
+                if "base_url" in chatcompletion_kwargs:
+                    base_url = chatcompletion_kwargs.pop("base_url", None)
+                else:
+                    base_url = chatcompletion_kwargs.pop("api_base", None)
+                api_key = chatcompletion_kwargs.pop("api_key", None)
+                chatcompletion_kwargs.update(kwargs)
+                chatcompletion_kwargs.pop("schema_error_retry", None)
+                completions = client.chat.completions.create(**chatcompletion_kwargs)
+                response = completions.model_dump()
+            else:
+                raise e
+
         return response
